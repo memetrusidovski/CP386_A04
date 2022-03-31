96dbeb1862c6dde61bd449946ffa4e3ae33c13c0
Met <memetrusidovski@gmail.com>
2022-03-30 20:44:14 -0400

    files

---
+++ Question1.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <semaphore.h>
+
+sem_t *mutex;
+
+void *threadRun(void *t);
+
+int main(int argc, char *argv[])
+{
+
+}
