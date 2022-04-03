d7a45241ab12861c5ed064ed10cae8ca7dd03bf0
Met <memetrusidovski@gmail.com>
2022-04-03 18:14:59 -0400

    testing

--- Question1.c
+++ Question1.c
@@ -10,6 +10,8 @@
 sem_t *mutex;
 char *name = "file.txt";
 
+int available[5][4] = {{6, 4, 7, 3}, {4, 2, 3, 2}, {2, 5, 3, 3}, {6, 3, 3, 2}, {5, 5, 7, 5}};
+
 void *threadRun(void *t);
 int readFile(char *fileName, int ***resources);
 
@@ -17,6 +19,8 @@ int main(int argc, char *argv[])
 {
     int ***resources = (int ***) malloc(sizeof(int));
     readFile(name, resources);
+    int done = 0;
+    char input[20];
 
     int numberCustomers = 0;
     int availableResources[4] = {0,0,0,0};
@@ -30,8 +34,16 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    while (done == 0)
+    {
+        printf("Enter Command: ");
+        scanf("%s", input);
+        printf(" \n");
 
 
+        if(strcmp(input, "exit") == 0) done = 1;
+        sleep(1);
+    }
 }
 
 int readFile(char *fileName, int ***resources) // use this method in a suitable way to read file
@@ -45,35 +57,41 @@ int readFile(char *fileName, int ***resources) // use this method in a suitable 
 
     char line[256];
 
-    while (fgets(line, sizeof(line), file))
-    {
-       printf("%s", line);
-    }
 
-    fclose(file);
-
-    *resources = (int **)malloc(sizeof(int) * 25);
+    //Build a 2d array to store values
+    int **tempresources = (int **)malloc(sizeof(int) * 25);
 
     int c = 0;
 
     while(c < 5){
         int *temp = (int *)malloc(sizeof(int)*5);
         
-        (*resources)[c] = temp;
+        tempresources[c] = temp;
         c++;
     }
 
     int x = 0;
     int y = 0;
 
-    while(x < 5){
-        while (y < 5){
-            printf("%d ", (*resources)[x][y]);
-            y++;
+    *resources = tempresources;
+
+    //(*resources)[1][1] = 4;
+    //printf("%d ", (*resources)[4][4]);
+
+
+    //Load data
+    while (fgets(line, sizeof(line), file))
+    {
+        char *token = NULL;
+        int j = 0;
+        token = strtok(line, " ");
+        while(token != NULL){
+            printf("%c ", token[0]);
+            token = strtok(NULL, " ");
         }
-        printf("\n");
-        x++;
+
     }
 
+    fclose(file);
     return 1;
 }
